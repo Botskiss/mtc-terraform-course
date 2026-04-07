1c9ec30e9a46f21bf4bcf54408f6b7938e5999c2
Botskiss <alfredomilad@yahoo.com>
2026-04-07 04:06:26 +0000

    Refresh Only

--- terraform-code/terraform.tfvars
+++ terraform-code/terraform.tfvars
@@ -1,3 +1,3 @@
 repo_count = 2
 #varsource  = "terraform.tfvars"
-
+env = "dev"
--- terraform-code/variables.tf
+++ terraform-code/variables.tf
@@ -1,8 +1,40 @@
 variable "repo_count" {
   type        = number
   description = "Number or repositories."
-  default     = 2
+  default     = 1
+
+  validation {
+    condition     = var.repo_count < 5
+    error_message = "Do no deploy more than 5 repositories."
+  }
+
 }
+
+variable "env" {
+  type        = string
+  description = "Deployment environment"
+  validation {
+    condition     = contains(["dev", "prod"], var.env)
+    error_message = "Env must be 'dev' or 'prod'"
+  }
+}
+
+
+
+
+
+
+
+
+# Will procduce an error!
+# variable "visibility" {
+#   type = string
+#   description = "Visibility of the repo."
+#   default = var.env == "dev" ? "private" : "public"
+# }
+
+
+
 
 # variable "varsource" {
 #   type        = string
