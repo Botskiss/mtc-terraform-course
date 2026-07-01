f28ad5eeaa5a72d851ef8478c7adef11be7d71ac
Alfredo Milad <alfredomilad@yahoo.com>
2026-07-01 10:03:37 +0000

    After route table association

--- aws-tf/modules/infra/main.tf
+++ aws-tf/modules/infra/main.tf
@@ -52,3 +52,8 @@ resource "aws_subnet" "this" {
   }
 }
 
+resource "aws_route_table_assocition" "this" {
+  for_each = aws_subnet.this
+  subnet_id = aws_subnet.this[each.key].id 
+  route_table_id = aws_route_table.this.id 
+}
