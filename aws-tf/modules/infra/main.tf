cc6158a1b4cc806244e830db1de79bbeba5f5935
Alfredo Milad <alfredomilad@yahoo.com>
2026-07-01 04:32:19 +0000

    Before "Subnets"

--- aws-tf/modules/infra/main.tf
+++ aws-tf/modules/infra/main.tf
@@ -4,3 +4,29 @@ resource "aws_vpc" "this" {
     Name = "mtc-ecs"
   }
 }
+
+resource "aws_internet_gateway" "this" {
+
+  tags = {
+    Name = "mtc-ecs"
+  }
+}
+
+resource "aws_internet_gateway_attachment" "this" {
+  internet_gateway_id = aws_internet_gateway.this.id
+  vpc_id              = aws_vpc.this.id
+}
+
+resource "aws_route_table" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "mtc-ecs"
+  }
+}
+
+resource "aws_route" "this" {
+  route_table_id         = aws_route_table.this.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
