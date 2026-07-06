4e02aad9ad876539de6c775b058c4b093b2a1d4a
Alfredo Milad <alfredomilad@yahoo.com>
2026-07-06 11:09:56 +0000

    before: CLI Workspaces

--- aws-tf/backends.tf
+++ aws-tf/backends.tf
@@ -1,12 +1,20 @@
+#terraform {
+#  cloud {
+
+#   organization = "botskiss-terraform"
+
+#  workspaces {
+#   name = "ecs"
+# }
+# }
+#}
+
 terraform {
-  cloud {
-
-    organization = "botskiss-terraform"
-
-    workspaces {
-      name = "ecs"
-    }
+  backend "s3" {
+    bucket       = "mtc-botskiss-app-state21125"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
   }
 }
-
 
