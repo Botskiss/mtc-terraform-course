41a5cfc00e7740b9de74b080c20ca4275a33a13f
Alfredo Milad <alfredomilad@yahoo.com>
2026-07-06 18:35:29 +0000

    Before: Terraform Import

--- aws-tf/backends.tf
+++ aws-tf/backends.tf
@@ -11,7 +11,7 @@
 
 terraform {
   backend "s3" {
-    bucket       = "mtc-botskiss-app-state21125"
+    bucket       = ""
     key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
